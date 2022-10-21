e6282cc2edc910c812294d56ceff7354961d7b7f
Joseph Howlett <JosephJHowlett@gmail.com>
2022-10-21 14:26:11 -0700

    update for class usage

--- rudimentary_sweep.py
+++ rudimentary_sweep.py
@@ -1,4 +1,4 @@
-from stepperMotorCtrl_moku import *
+import stepperMotorCtrl
 from pexpect import pxssh
 import time
 
@@ -20,59 +20,42 @@ def remote_timed_daq(time_s, velocity, name):
     return
 
 if __name__=='__main__':
-    name = '20221020_1548_co57'
-    velocities = np.array([-0.4, -0.3, -0.2, -0.159, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4])
-    velocities = np.array([-1.0, 1.0, -1.0, 1.0, -1.0, 1.0])
+    name = '20221021_1412_co57'
+    #velocities = np.array([-0.4, -0.3, -0.2, -0.159, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4])
+    #velocities = np.array([-1.0, 1.0, -1.0, 1.0, -1.0, 1.0])
+    velocities = np.array([-0.2])
+    scan = stepperMotorCtrl.ScanController(
+        mokuWGChannel=2,
+        commandSleepTime=1,
+        scanTravelDist=40,
+        returnVelocity=5
+    )
 
-    moku = mokuGO('192.168.73.1')
     # Turn on the base high voltage
-    moku.BaseHVon(1, 1) # Default to 1kV bias on channel 1
+    scan.moku.setBaseHV(1, 1) # Default to 1kV bias on channel 1
 
-    scan_times = D_TRAVEL / np.abs(velocities)
+    scan_times = scan.scanTravelDist / np.abs(velocities)
     scan_times[scan_times==np.inf] = scan_times.min()
     print('\nTotal scanning time (s): ', scan_times.sum(), '\n')
 
-    ctrl = DFR1507A()
-    ctrl.AWoff()
-    ctrl.setDirection('CCW')
-    ctrl.setResolution(1)
-    time_s = int(0.9 * D_TRAVEL / np.max(np.abs(velocities)))
+    time_s = int(0.9 * scan.scanTravelDist / np.max(np.abs(velocities)))
     print('\n\n\nDAQ time (s): ', time_s, '\n\n')
     for i, vel in enumerate(velocities):
         print(f'initiating scan at {vel} mm/s')
-        if vel<0:
-            returnVel = V_RETURN
-        else:
-            returnVel = -V_RETURN
-        #tScan = D_TRAVEL/np.abs(vel) # seconds to scan
         tScan = scan_times[i]
         print(f'scanning for {tScan} seconds')
-        tReturn = D_TRAVEL/V_RETURN
-        if vel==0:
-            tReturn = 0
-        ctrl.setVelocity(vel)
-        # funcGen.setFreq(ctrl.fStep)
-        moku.PWMon(ctrl.fStep, 2) # Defaults to use channel 2
         start_time = time.time()
-        ctrl.AWon()
-        time.sleep(1)
+        scan.start_step(vel)
         print('sending DAQ command via ssh')
         # TODO: move to subprocess
         remote_timed_daq(time_s, vel, name)
         while time.time() < (start_time + tScan):
             time.sleep(1)
-        ctrl.AWoff()
-        moku.PWMoff(2)
-        if tReturn:
-            # And then go back to where we started.
-            ctrl.setResolution(2)
-            ctrl.setVelocity(returnVel)
-            moku.PWMon(ctrl.fStep,2)
-            ctrl.AWon()
-            time.sleep(1)
-            time.sleep(tReturn)
-            ctrl.AWoff()
-            moku.PWMoff()
-            time.sleep(1)
-            ctrl.setResolution(1) # Reset to the fine scan...
+        scan.stopMotion()
+        tReturn = self.scanTravelDist/self.returnVelocity
+        scan.step(
+            -1 * self.returnVelocity * np.sign(vel),
+            self.scanTravelDist, 
+            2
+        )
     print('scan done')
