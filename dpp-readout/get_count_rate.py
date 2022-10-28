66a368980e6685f0b8b00e738515952139dfbe66
Joseph Howlett <JosephJHowlett@gmail.com>
2022-10-28 14:10:31 -0700

    122keV

--- dpp-readout/get_count_rate.py
+++ dpp-readout/get_count_rate.py
@@ -15,13 +15,16 @@ try:
 except IndexError:
     print('Usage: python get_count_rate.py <time (seconds)> <velocity (mm/s)> <name (e.g. YYYYMMDD_i)>')
     sys.exit()
+if len(sys.argv)>4:
+    assert sys.argv[4]=='122', "must select 14 or 122 keV"
+    config_file = 'co57_config_122keV_10dB_attenuated'
 
 data_file = join(datadir, name + '.dat')
 if not os.path.exists(data_file):
     with open(data_file, 'w') as f:
         f.write('vel\tcount\tseconds\tstart-time\t\tstop-time\n')
 
-digitizer = DPPReadout(join(daqdir, config_file), verbose=True)
+digitizer = DPPReadout(join(daqdir, config_file), verbose=False)
 digitizer.timed_acquire(time_s)
 count = digitizer.count
 stop_time = time.time()
