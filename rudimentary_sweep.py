70da918e1cf844ef12e00b4d96b99af2caa1f9cf
Joseph Howlett <JosephJHowlett@gmail.com>
2022-10-28 14:05:10 -0700

    optional 122keV setting

--- rudimentary_sweep.py
+++ rudimentary_sweep.py
@@ -3,9 +3,11 @@ from pexpect import pxssh
 import time
 import numpy as np
 
-def remote_timed_daq(time_s, velocity, name):
+def remote_timed_daq(time_s, velocity, name, high_e=False):
     dataserver_daq_script = '/home/joeyh/daq/dpp-pha/dpp-readout/get_count_rate.py'
-    cmd = f'python {dataserver_daq_script} {time_s} {velocity} {name} >> /home/joeyh/daqlog.log'
+    line = {True: '122', False: ''}[high_e]
+    cmd = f'python {dataserver_daq_script} {time_s} {velocity} {name} {line} >> /home/joeyh/daqlog.log'
+    print(cmd)
     try:
         s = pxssh.pxssh(timeout=time_s*2)
         hostname = 'mossbauer-dataserver.stanford.edu'
@@ -21,8 +23,9 @@ def remote_timed_daq(time_s, velocity, name):
     return
 
 if __name__=='__main__':
-    name = '20221026_0926'
-    velocities = np.array([-1.0, -0.4, -0.3, -0.2, -0.159, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 1.0])
+    name = '20221028_1333'
+    high_e = False  # set True for 122 keV config
+    velocities = np.array([-1.0, -0.8, -0.6, -0.5, -0.4, -0.3, -0.2, -0.159, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1.0])
 
     scan = stepperMotorCtrl.ScanController(
         mokuWGChannel=2,
@@ -33,6 +36,7 @@ if __name__=='__main__':
     )
 
     # Turn on the base high voltage
+    # TODO: check HV instead of setting it?
     scan.moku.setBaseHV(1, 1) # Default to 1kV bias on channel 1
     scan_times = scan.scanTravelDist / np.abs(velocities)
     scan_times[scan_times==np.inf] = 100  # fix v=0 case
@@ -50,7 +54,7 @@ if __name__=='__main__':
         start_time = time.time()
         scan.start_step(vel)
         print('sending DAQ command via ssh')
-        remote_timed_daq(daq_times[i], vel, name)
+        remote_timed_daq(daq_times[i], vel, name, high_e)
         while time.time() < (start_time + tScan):
             time.sleep(1)
         scan.stopMotion()
