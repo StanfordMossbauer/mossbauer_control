0920a2c9d146b8d68f64129118c7a72ed8dfd668
unknown <anazeeri@stanford.edu>
2023-03-10 16:25:58 -0800

    added x2 flag functionality

--- mossbauer_stepper_controller_firmware/mossbauer_stepper_controller_firmware.ino
+++ mossbauer_stepper_controller_firmware/mossbauer_stepper_controller_firmware.ino
@@ -5,8 +5,11 @@ int PLS_PIN = 11; //red wire, is not currently used in code
 int RS2_PIN = 12; //blue wire
 char serialchar = '\0';
 
-bool flagA = false;
-bool flagB = false;
+int FLAG0_PIN = 2;
+int FLAG1_PIN = 3;
+
+int flag0_state = LOW;
+int flag1_state = LOW;
 
 void setup() {
   // put your setup code here, to run once
@@ -16,9 +19,14 @@ void setup() {
   pinMode(CCW_PIN, OUTPUT);
   pinMode(PLS_PIN, OUTPUT); //not currently used
   pinMode(RS2_PIN, OUTPUT);
+  pinMode(FLAG0_PIN, INPUT);
+  pinMode(FLAG1_PIN, INPUT);
 }
 
 void loop() {
+  flag0_state = digitalRead(FLAG0_PIN);
+  flag1_state = digitalRead(FLAG1_PIN);
+
   if (Serial.available() > 0){
     serialchar = Serial.read();
     if (serialchar == 'a'){
@@ -40,7 +48,21 @@ void loop() {
       digitalWrite(AW_PIN, LOW);
     }
     if (serialchar == '?'){
-      Serial.println("microcontroller has no answers");
+      if ((flag0_state == LOW) && (flag1_state == LOW)){
+        Serial.println("w");
+      }
+      else if ((flag0_state == HIGH) && (flag1_state == LOW)){
+        Serial.println("x");
+      }
+      else if ((flag0_state == LOW) && (flag1_state == HIGH)){
+        Serial.println("y");
+      }
+      else if ((flag0_state == HIGH) && (flag1_state == HIGH)){
+        Serial.println("z");
+      }
+      else{
+        Serial.println("flag read failure");
+      }
     }
   }
 
