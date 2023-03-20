c24a123c075bcff35b2b8904785ae774dc1df75e
StanfordMossbauer <StanfordMossbauer@gmail.com>
2023-03-20 14:41:04 -0700

    package working

--- mossbauer_control/motor.py
+++ mossbauer_control/motor.py
@@ -1,28 +1,9 @@
 import pyvisa
 import sys, os
 import numpy as np
-import logging
 
 from mossbauer_control.instruments import Agilent, DFR1507A
-
-
-class StreamToLogger(object):
-    """Fake file-like stream object that redirects writes to a logger instance.
-
-    from https://stackoverflow.com/questions/19425736/how-to-redirect-stdout-and-stderr-to-logger-in-python
-    """
-    def __init__(self, logger, level):
-       self.logger = logger
-       self.level = level
-       self.linebuf = ''
-       return
-
-    def write(self, buf):
-       for line in buf.rstrip().splitlines():
-          self.logger.log(self.level, line.rstrip())
-
-    def flush(self):
-        pass
+from mossbauer_control.utils import *
 
 
 class Motor:
