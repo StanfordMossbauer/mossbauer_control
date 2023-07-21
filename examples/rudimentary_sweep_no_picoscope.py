6db041d7d1b98c654b584de478e15e17442338ba
Joseph Howlett <JosephJHowlett@gmail.com>
2023-07-21 09:48:34 -0700

    Revert "fix conflict"
    This reverts commit b70b92fb1fb7c7d8c3fdff7cb23bb60e3ef08937, reversing
    changes made to a4483d06286491fa35897059e42bdbc165085070.

--- examples/rudimentary_sweep_no_picoscope.py
+++ examples/rudimentary_sweep_no_picoscope.py
@@ -15,7 +15,6 @@ directory = "/home/mossbauer/Data/{}_scan/".format(time.strftime("%Y%m%d"))
 #data_file = directory + 'Fe0004_1_mms_0.05_mms_19in.dat'
 #data_file = directory + 'Fe0004_1_mms_0.05_mms_19in_cardboard.dat'
 data_file = directory + 'FeCy_0.6_mms_9steps_0.6-17in.dat'
-data_file = directory + 'Fe0004_0vel_1step_0.6-17in_noskim_darknoise.dat'
 data_file = directory + 'Fe0004_0.9_mms_13steps_0.6-17in.dat'
 #data_file = directory + 'Fe0004_0.6_mms_outersteps_0.6-17in.dat'
 
@@ -32,7 +31,6 @@ Xmin = 0e-3
 
 frequencies = np.linspace(0, 1, 9)#[np.linspace(0,0.5,51)[21]]#np.ones(5)# np.linspace(0, 150, 4)#
 frequencies = np.linspace(0, 1.5, 13)#[-4:]
-#frequencies = np.array([0.1,0.1])#np.linspace(0, 1.5, 13)#[-4:]
 #scanTime = 3/frequencies[1]
 scanTime = 24
 if scanTime<(1/frequencies[1:].min()): print("Careful too shot time for lower frequency")
@@ -153,101 +151,9 @@ def write_results(frequency, results, data_file, print_results=True):
     return
 
 for j in range(repetitions):
-    for i,frequency in enumerate(frequencies):
+    for frequency in frequencies:
         if frequency==0:
             results = zero_velocity_sweep()
         else:
-            piezo.active = 1
-            print('reset piezo')
-            if piezo.output=='OFF':
-                piezo.output = 'ON'
-
-        period = 1/frequency
-        cycles = int(scanTime/period)
-        actualscanTime = cycles*period
-
-        #switch upstream counting or not
-        skim.burstcycles = 2*cycles
-        skim.frequency = 2*frequency
-
-        #switch downstream: choosing channel
-        piezo.active = 2 #switch
-        piezo.burstcycles = cycles   
-        piezo.frequency = frequency
-
-        #stage moving otr not
-        piezo.active = 1 #position voltage
-        piezo.burstcycles = cycles   
-        piezo.frequency = frequency
-####TODO REMOVE#####
-##### setting all speeds to zero
-       # if piezo.output=='ON': piezo.output = 'OFF';
-
-        caen.start() #it takes a bit, ideally we have also a switch after this so we can wait!
-        time.sleep(1)  # TODO: remove when everything working
-        clock.device.write('*TRG')
-
-        print('sleep for %d secs' % actualscanTime)
-        time.sleep(actualscanTime)
-        time.sleep(1)  # TODO: remove when everything working
-        caen.stop()
-        caen.update_count()
-        caen.histogram()
-
-
-
-        count_list_ch_0.append(caen.count[0] - prev_count[0])
-        count_list_ch_1.append(caen.count[1] - prev_count[1])
-
-        prev_count[0] = caen.count[0]
-        prev_count[1] = caen.count[1]
-        
-        # this is the time that gets used to calculate the rate
-        actual_DAQTime_list.append(actualscanTime/2*dutypercent/100)
-
-        data = [np.ones(1000),np.ones(1000)]
-
-        #nominal velocity
-        nominal_velocity_list_ch_0.append(2*(Xmax-Xmin)*frequencies[i])
-        nominal_velocity_list_ch_1.append(-2*(Xmax-Xmin)*frequencies[i])
-
-        #actual velocity (fit to strain gauge reading)
-        tt = np.arange(len(data[0])//2)*si[0]
-        x_p = data[0][:len(data[0])//2]*(20/2**16)*(Xmax-Xmin)/(Vmax-Vmin)
-        x_n = data[0][len(data[0])//2:]*(20/2**16)*(Xmax-Xmin)/(Vmax-Vmin)
-
-        p0 = [-0.1, 0.2]
-        p_p,dp_p,res_p = m.fit_residuals(lin, tt, x_p, p0, fullout = False)
-
-        p0[1] = -p_p[0]
-        p_n,dp_n,res_n = m.fit_residuals(lin, tt, x_n, p0, fullout = False)
-
-        actual_velocity_list_ch_0.append(p_p[1])
-        v_mean_squared_residual_ch_0.append(res_p)
-
-        actual_velocity_list_ch_1.append(p_n[1])
-        v_mean_squared_residual_ch_1.append(res_n)
-        #plt.plot(tt,x_p, '.')
-        #plt.plot(tt,lin(p_p,tt), '.')
-
-        #t5 = time.time()
-
-        with open(data_file, 'a') as f:
-                    f.write(f'{nominal_velocity_list_ch_0[-1]}\t{actual_velocity_list_ch_0[-1]}\t{v_mean_squared_residual_ch_0[-1]}\t{count_list_ch_0[-1]}\t{actual_DAQTime_list[-1]}\n')
-                    f.write(f'{nominal_velocity_list_ch_1[-1]}\t{actual_velocity_list_ch_1[-1]}\t{v_mean_squared_residual_ch_1[-1]}\t{count_list_ch_1[-1]}\t{actual_DAQTime_list[-1]}\n')
-
-        #print('full scans: {}/{}, current iteration {}/{},  +/-{:.2f} mm/s ch_0: {:.2f} Hz ch_1: {:.2f} Hz'.format(j,repetitions, i,len(frequencies), actual_velocity_list_ch_0[-1], count_list_ch_0[-1]/scanTime, count_list_ch_1[-1]/scanTime))
-        #print(t1-t0,t2-t1,t3-t2,t4-t3,t5-t4,time.time()-t5)
-        print('full scans: {}/{}, current frequency {:.3f},  +/-{:.2f} mm/s ch_0: {:.2f} Hz ch_1: {:.2f} Hz ratio: {:.2f}'.format(j,repetitions,frequencies[i], nominal_velocity_list_ch_0[-1], count_list_ch_0[-1]/actual_DAQTime_list[-1], count_list_ch_1[-1]/actual_DAQTime_list[-1], count_list_ch_0[-1]/count_list_ch_1[-1]))
-        
-
-#plt.show()
-
-for piezo.active in [1,2]:
-    piezo.output = "OFF"
-clock.output = 'OFF'
-skim.output = 'OFF'
-piezo.close()
-clock.close()
-caen.close()
-#ps.close()
+            results = sweep(frequency)
+        write_results(frequency, results, data_file)
