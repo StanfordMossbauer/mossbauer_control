f691e0e61583eb250664a8799ed3fb6a99bc022d
Euchupi <youngroosevelt@163.com>
2025-10-13 20:37:56 -0700

    yocoscope debug

--- mossbauer_control/instruments/Yoctopuce.py
+++ mossbauer_control/instruments/Yoctopuce.py
@@ -1,166 +1,127 @@
-
-
+from os import path, makedirs
+import csv
 from yoctopuce.yocto_api import *
 from yoctopuce.yocto_temperature import *
 from yoctopuce.yocto_humidity import *
 from yoctopuce.yocto_pressure import *
+
 from datetime import datetime
-import pandas as pd
-import matplotlib.pyplot as plt
-import csv
+#import schedule
+import time
 
 
+class Yoctopuce():
+    def __init__(self, handle, logfile = 'yoctolog_'+datetime.now().strftime('%Y%m%d')+'.csv', layout=None):
+        # logger class with init and update methods
+        # input: logger's name/handle (METEO...), log file w/ extension, parent's layout
 
+        # file name and dir
+        self.log_file = logfile
 
-class Yoctopuce:
-	def __init__(self, address):
-
-		errmsg = YRefParam()
-		if YAPI.RegisterHub("usb", errmsg) != YAPI.SUCCESS:
-			#sys.exit("init error :" + errmsg.value)
-			print("error :" + errmsg.value)
-
-		print(address)
-		
-		self.temperature = YTemperature.FindTemperature(address +'.temperature')
-		self.humidity = YHumidity.FindHumidity(address+'.humidity')
-		self.pressure = YPressure.FindPressure(address+'.pressure')
-
-	def __del__(self):
-		print("Closing yoctopuce")
-
+        errmsg = YRefParam()
+        if YAPI.RegisterHub("usb", errmsg) != YAPI.SUCCESS:
         #if YAPI.RegisterHub("127.0.0.1", errmsg) != YAPI.SUCCESS:
             # use YAPI.RegisterHub("127.0.0.1", errmsg) if dev is connected to the same pc
             # if this failed, try YAPI.RegisterHub("usb", errmsg)
+            sys.exit("init error :" + errmsg.value)
+        try:
+            self.temperature = YTemperature.FindTemperature(handle+'.temperature')
+
+            self.tempFound = True
+            self.tempstatus = ""
+        except Exception as e:
+            self.tempFound = False
+            print(e)
+            self.tempstatus = "Temp. sensor error."
+
+        try:
+            self.humidity = YHumidity.FindHumidity(handle+'.humidity')
+
+            self.humFound = True
+            self.humstatus = ""
+        except Exception as e:
+            self.humFound= False
+            print(e)
+            self.humstatus = "Humidity sensor error."
+
+        try:
+            self.pressure = YPressure.FindPressure(handle+'.pressure')
+            self.presFound = True
+            self.presstatus = ""
+        except Exception as e:
+            self.presFound= False
+            print(e)
+            self.presstatus = "Humidity sensor error."
+
+        # init time array in main 
+        self.times = [datetime.now().timestamp()]
+        # init TPH arrays in main 
+        self.Tvals=[self.temperature.get_currentValue()]
+        self.Pvals=[self.pressure.get_currentValue()]
+        self.Hvals=[self.humidity.get_currentValue()]
 
 
-
+    def update(self):
+        """Updates TPH and time. save to file."""
+        try:
+            currentT= self.temperature.get_currentValue()
+            currentH=self.humidity.get_currentValue()
+            currentP=self.pressure.get_currentValue()
+            currenttime = datetime.now().timestamp()
+            timedisplay= datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        except:
+            YAPI.Sleep(1000)
+            print('update error. try again...')
+            currentT= self.temperature.get_currentValue()
+            currentH=self.humidity.get_currentValue()
+            currentP=self.pressure.get_currentValue()
+            currenttime = datetime.now().timestamp()
+            timedisplay= datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             
-        
+        self.Tvals.append(currentT)
+        self.Pvals.append(currentP)
+        self.Hvals.append(currentH)
+        self.times.append(currenttime)
 
+        ## Save to file. Append if exists, otherwise create.
+        if path.exists(self.log_file):
+            type = 'a'
+        else:
+            type = 'w'
+            with open(self.log_file, type, newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['time', 'temp [C]', 'RH [%]', 'P [mbar]'])
 
-if __name__=='__main__':
-	address = 'METEOMK2-2377A2'
-	yct1= Yoctopuce(address)
-
-# 	T_list = []
-# 	P_list = []
-# 	H_list = []
-# 	timestamps = []
-# 	start = time.time()
-# 	i=1
-# 	while True:
-# 		i+=1
-# 		time.sleep(1)
-# 		T_list.append(yct1.temperature.get_currentValue())
-# 		H_list.append(yct1.humidity.get_currentValue())
-# 		P_list.append(yct1.pressure.get_currentValue())
-# 		timestamps.append(time.time()-start)
-
-# 	df=pd.DataFrame(
-# 		{'time':timestamps,
-# 		'T':T_list,
-# 		'P':P_list,
-# 		'H':H_list}
-# 		)
-
-# 	# Define the full file path
-# 	save_path = r'C:\Users\mossbauer\Documents\data\gpib-isolator_0620\test_keithley_straingauge_nocurrentsource_2_yocto.csv'
-# 	os.makedirs(os.path.dirname(save_path), exist_ok=True)
-# 	df.to_csv(save_path, index=False)
-
-
-
-# # Base save directory
-# base_dir = r'C:\Users\mossbauer\Documents\data\1008'
-# os.makedirs(base_dir, exist_ok=True)
-
-# # Start time
-# start = time.time()
-
-# # Helper function to generate filename for the current date
-# def get_filename():
-#     date_str = datetime.now().strftime('%Y-%m-%d')
-#     return os.path.join(base_dir, f'{date_str}_yocto_data.csv')
-
-# # Initial filename and file setup
-# current_date = datetime.now().date()
-# file_path = get_filename()
-# file = open(file_path, 'a')
-
-# # Write header if file is new
-# if os.stat(file_path).st_size == 0:
-#     file.write('time,T,P,H\n')
-
-# print(f"Logging to: {file_path}")
-
-# try:
-#     while True:
-#         time.sleep(1)
-#         now = datetime.now()
-#         new_date = now.date()
-
-#         # Check if date has changed
-#         if new_date != current_date:
-#             file.close()
-#             current_date = new_date
-#             file_path = get_filename()
-#             file = open(file_path, 'a')
-#             file.write('time,T,P,H\n')  # new file = write header
-#             print(f"Date changed. Logging to new file: {file_path}")
-
-#         current_time = time.time() - start
-#         T = yct1.temperature.get_currentValue()
-#         H = yct1.humidity.get_currentValue()
-#         P = yct1.pressure.get_currentValue()
-
-#         # Write new row
-# 		with open(file_path, mode='a', newline='') as file:
-#         	writer = csv.writer(file)
-#         	#writer.writerow([timestamp, data_V, data_T, R, theta])
-#         	writer.writerow([timestamp, T, H, P])
-
-
-def get_file_path(base_dir):
-    """Generate file path with current date"""
-    date_str = datetime.now().strftime("%Y-%m-%d")
-    filename = f"all_closed{date_str}.csv"
-    return os.path.join(base_dir, filename)
-
-# Folder to save CSVs
-base_dir = r'C:\Users\mossbauer\Documents\data\1008'
-os.makedirs(base_dir, exist_ok=True)
-
-# Track current file date
-current_day = datetime.now().date()
-file_path = get_file_path(base_dir)
-
-# Write header once per file
-with open(file_path, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['time', 'T',  'H', 'P'])
-
-while True:
-    # Check if the date has changed
-    now = datetime.now()
-    if now.date() != current_day:
-        # New day, create a new file
-        current_day = now.date()
-        file_path = get_file_path(base_dir)
-        with open(file_path, mode='w', newline='') as file:
+        with open(self.log_file, type, newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['time', 'T', 'H', 'P'])
+            writer.writerow([timedisplay, currentT, currentH, currentP])
+           
+        
+def updateall(loggers, closingtime='0100'):
+    ''' update loggers via schedule'''
+    
+    today =datetime.now()   
+    for l in loggers:
+        l.update()
+        
+    if today.strftime('%H%M')==closingtime:
+        #clear the task every day at closing time 
+        schedule.clear()
+        print('cancelling at (mdHM)'+today.strftime('%m%d%H%M'))
+        
+        # cleaning up ...
+        YAPI.UpdateDeviceList()
+        YAPI.HandleEvents()
+        sys.exit()
+    
+if __name__ == "__main__":
+    
+    loggername_mossbauer = 'METEOMK2-2377A2'
 
-    # Sleep before getting data
-    time.sleep(1)
 
-    # Get data
-    timestamp = time.time()
-    T = yct1.temperature.get_currentValue()
-    H = yct1.humidity.get_currentValue()
-    P = yct1.pressure.get_currentValue()
-
-    # Write data to file
-    with open(file_path, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([timestamp, T, H, P])
+    Y = yoctopucelogger(loggername_mossbauer)
+    T  = Y.temperature.get_currentValue()
+    P  = Y.pressure.get_currentValue()
+    H  = Y.humidity.get_currentValue()
+    
+ 
