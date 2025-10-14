15cb0449e1dd494eeb570528a0d8d056a384d59a
Euchupi <youngroosevelt@163.com>
2025-10-13 22:47:56 -0700

    debug

--- scripts/mossbauer_data_s.py
+++ scripts/mossbauer_data_s.py
@@ -15,7 +15,7 @@ from mossbauer_control.instruments import SRS830
 
 def insert_fast(cursor, t_dt, data_V, R, theta):
 	cursor.execute(
-		"INSERT INTO FAST (`TIME`, `VOLTMETER`, `SRS_R`,`SRS_THETA`) VALUES (%s, %s, %s)",
+		"INSERT INTO FAST (`TIME`, `VOLTMETER`, `SRS_R`,`SRS_THETA`) VALUES (%s, %s, %s, %s)",
 		(t_dt, float(data_V),float(R), float(theta))
 	)
 
@@ -45,71 +45,6 @@ voltmeter.clear_buffer()
 voltmeter.store_raw_readings()
 voltmeter.cont_operation()
 voltmeter.initialize()
-#thermo.clear_buffer()
-#thermo.store_raw_readings()
-#thermo.cont_operation()
-#thermo.initialize()
-	
-# data_V = []
-# data_T = []
-# data_theta = []
-# data_amp = []
-# timestamps = []
-# start = time.time()
-
-# i = 1
-# while i<3:
-#     i+=1
-#     time.sleep(60)
-#     data = voltmeter.get_data()
-#     data_V.append(data)
-#     data = thermo.get_data()
-#     data_T.append(data)
-#     timestamps.append(time.time())
-#     R,theta, f_ref = srs.read_all()
-#     data_amp.append(R)
-#     data_theta.append(theta)
-
-# df=pd.DataFrame(
-#     {'time':timestamps,
-#     'data V':data_V,
-#     'data T': data_T,
-#     'amp': data_amp,
-#     'theta': data_theta}
-#     )
-
-
-
-# # Define the full file path
-# save_path = r'C:\Users\mossbauer\Documents\data\0919\all_02.csv'
-# os.makedirs(os.path.dirname(save_path), exist_ok=True)
-# df.to_csv(save_path, index=False)
-
-
-# save_path = r'C:\Users\mossbauer\Documents\data\0922\all_01.csv'
-# os.makedirs(os.path.dirname(save_path), exist_ok=True)
-
-# # Initialize file with header
-# columns = ['time', 'data V', 'data T', 'amp', 'theta']
-# df_empty = pd.DataFrame(columns=columns)
-# df_empty.to_csv(save_path, index=False)
-
-# i = 0
-# while i < 3:
-#     i += 1
-#     time.sleep(60)
-
-#     # Gather data
-#     data_V = voltmeter.get_data()
-#     data_T = thermo.get_data()
-#     timestamp = time.time()
-#     R, theta, f_ref = srs.read_all()
-
-#     # Create one-row DataFrame
-#     df_row = pd.DataFrame([[timestamp, data_V, data_T, R, theta]], columns=columns)
-
-#     # Append to CSV
-#     df_row.to_csv(save_path, mode='a', header=False, index=False)
 
 
 #or we can create a new csv file every day
